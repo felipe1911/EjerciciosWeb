934357b1f694103445ed4735a34283ea58e41151
felipe1911 <fellipemunozvelez@gmail.com>
2023-11-01 12:50:55 -0500

    intro js

---
+++ assets/js/intrduccion.js
@@ -0,0 +1,9 @@
+let a = 5
+let b = 10
+
+console.log(`La suma es ${a+b}`)
+
+let num1 = parseInt(prompt('Digite el primer número: '))
+let num2 = parseInt(prompt('Digite el segundo número: '))
+
+console.log(`El resultado es ${num1+num2}`)
